596bd98ab1efcf2d9e27e19dff236cdac1ea84d8
fakhbari-dev <f.akhbari72dev@gmail.com>
2023-12-31 13:31:05 +0330

    add installment plugin and add nedded changes in facilities plugin and data context

---
+++ installment/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import CustomizedTables from "./components/Table";
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <CustomizedTables />
+    </div>
+  );
+}
+
+export default App;
---
+++ installment/src/bootstrap.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {DataProvider} from "@banking/data-context";
+
+import App from './app/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <DataProvider>
+      <App />
+    </DataProvider>
+
+  </StrictMode>
+);
